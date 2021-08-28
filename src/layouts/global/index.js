d512b43b663a4982c2ff9a0b383b5bbda50be21b
함초롬 <chorom.m.ham@gmail.com>
2021-08-28 23:36:11 +0900

    💄 style: Add global container

--- src/layouts/global/index.js
+++ src/layouts/global/index.js
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+
 import GlobalHeader from "./header";
 import GlobalFooter from "./footer";
 
@@ -5,8 +7,14 @@ export default function GlobalLayout({ children }) {
   return (
     <>
       <GlobalHeader />
-      {children}
+      <Container>{children}</Container>
       <GlobalFooter />
     </>
   );
 }
+
+const Container = styled.div`
+  margin-top: 4.8rem;
+  display: flex;
+  flex-direction: column;
+`;
