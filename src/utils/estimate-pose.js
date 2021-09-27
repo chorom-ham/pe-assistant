7bf558ad83e8ae568492108f2082e6ec20a33b2d
Yoon Hajung <49065638+YHaJung@users.noreply.github.com>
2021-09-27 12:06:29 +0900

    🩹  Fix : change angle calculation

--- src/utils/estimate-pose.js
+++ src/utils/estimate-pose.js
@@ -1,4 +1,5 @@
 import hajung from "./detect-pose/hajung";
+import gymnastics from "./detect-pose/gymnastics";
 
 export function getKeypointsObject(pose) {
   return pose.keypoints.reduce((acc, { part, position }) => {
@@ -9,13 +10,15 @@ export function getKeypointsObject(pose) {
 
 export function getAngle(x1, y1, x2, y2) {
   const rad = Math.atan2(y2 - y1, x2 - x1);
-  return (rad * 180) / Math.PI;
+  return -1 * ((rad * 180) / Math.PI);
 }
 
 export default function EstimatePose(action) {
   switch (action) {
     case "hajung":
       return hajung();
+    case "gymnastics":
+      return gymnastics();
   }
   // return [count, step, checkPoses];
 }
