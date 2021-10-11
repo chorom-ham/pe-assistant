9a51218fd321156d004c5e0f6bba6b4bac97ab8f
Taehee-K <taehee107275@gmail.com>
2021-10-12 01:09:29 +0900

    ✨ feat: Count dumbbel arm exercise

--- src/utils/detect-pose/dumbellupper.js
+++ src/utils/detect-pose/dumbellupper.js
@@ -1,10 +1,14 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { getKeypointsObject, getAngle } from "../estimate-pose";
 
 // 추후 함수명은 동작 이름으로 변경. 대문자로 시작.
 export default function Dumbellupper() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(0);
+
+  const [leftDumbell, setLeftDumbell] = useState(false);
+  const [rightDumbell, setRightDumbell] = useState(false);
+
   const checkPoses = useCallback((pose) => {
     const {
       leftShoulder,
@@ -37,25 +41,33 @@ export default function Dumbellupper() {
       leftLow: getAngle(leftElbow.x, leftElbow.y, leftWrist.x, leftWrist.y),
     };
 
-    const start = checkDownRight(anglesArms) && checkDownLeft(anglesArms);
-
-    if (start) {
-      console.log("start");
-      setStep(1);
-    }
-
-    const up = checkUpLeft(anglesArms) || checkUpRight(anglesArms);
-
-    if (step === 1 && up) {
-      console.log("up");
-      setCount(count + 1);
-      setStep(0);
-    }
+    setLeftDumbell(checkLeftDumbell(anglesArms));
+    setRightDumbell(checkRightDumbell(anglesArms));
   });
+
+  useEffect(() => {
+    if ((step == 0 && leftDumbell) || (step == 1 && rightDumbell)) {
+      if (step == 0) console.log("left dumbell", count);
+      else if (step == 1) console.log("right dumbell", count);
+      setCount((count) => count + 1);
+    }
+  }, [step, count, leftDumbell, rightDumbell]);
+
+  useEffect(() => {
+    if (step == 0 && count > 20) {
+      setStep((step) => 1);
+      setCount((count) => 0);
+    }
+    if (step == 1 && count > 20) {
+      setStep((step) => 2);
+      setCount((count) => 0);
+    }
+  }, [step, count]);
+
   return [count, step, checkPoses];
 }
 
-function checkUpLeft(anglesArms) {
+function checkLeftDumbell(anglesArms) {
   if (anglesArms.leftHigh < 80) {
     return false;
   } else if (anglesArms.leftLow < 20) {
@@ -65,7 +77,7 @@ function checkUpLeft(anglesArms) {
   }
 }
 
-function checkUpRight(anglesArms) {
+function checkRightDumbell(anglesArms) {
   if (anglesArms.rightHigh < 80) {
     return false;
   } else if (anglesArms.rightLow < 20) {
@@ -75,22 +87,22 @@ function checkUpRight(anglesArms) {
   }
 }
 
-function checkDownLeft(anglesArms) {
-  if (anglesArms.leftHigh < 80) {
-    return false;
-  } else if (-80 < anglesArms.leftLow) {
-    return false;
-  } else {
-    return true;
-  }
-}
+// function checkDownLeft(anglesArms) {
+//   if (anglesArms.leftHigh < 80) {
+//     return false;
+//   } else if (-80 < anglesArms.leftLow) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
 
-function checkDownRight(anglesArms) {
-  if (anglesArms.rightHigh < 80) {
-    return false;
-  } else if (-80 < anglesArms.rightLow) {
-    return false;
-  } else {
-    return true;
-  }
-}
+// function checkDownRight(anglesArms) {
+//   if (anglesArms.rightHigh < 80) {
+//     return false;
+//   } else if (-80 < anglesArms.rightLow) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
