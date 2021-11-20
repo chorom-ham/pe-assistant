526f85753e00bf3c4cb384c222f23c8651023897
함초롬 <chorom.m.ham@gmail.com>
2021-11-20 20:43:43 +0900

    ✨ feat: Add api base

---
+++ src/hooks/api.js
@@ -0,0 +1,67 @@
+import axios from "axios";
+
+class RequestConfig {
+  baseURL;
+  headers;
+  method;
+  url;
+  data;
+
+  constructor(baseURL) {
+    this.baseURL = baseURL;
+  }
+
+  setToken(token) {
+    if (token) {
+      this.headers = {
+        Authorization: `Bearer ${token}`,
+      };
+    }
+  }
+
+  get(path, config) {
+    this.method = "GET";
+    this.url = this.baseURL + path;
+
+    return { ...config, ...this };
+  }
+
+  delete(path, config) {
+    this.method = "DELETE";
+    this.url = this.baseURL + path;
+
+    return { ...config, ...this };
+  }
+
+  post(path, data, config) {
+    this.method = "POST";
+    this.url = this.baseURL + path;
+    this.data = data;
+
+    return { ...config, ...this };
+  }
+
+  put(path, data, config) {
+    this.method = "PUT";
+    this.url = this.baseURL + path;
+    this.data = data;
+
+    return { ...config, ...this };
+  }
+
+  patch(path, data, config) {
+    this.method = "PATCH";
+    this.url = this.baseURL + path;
+    this.data = data;
+
+    return { ...config, ...this };
+  }
+}
+
+export const base = () => {
+  const requestConfig = new RequestConfig(
+    `https://api.airtable.com/v0/${process.env.AIRTABLE_ID}/LandingEmail`
+  );
+  requestConfig.setToken(process.env.AIRTABLE_KEY);
+  return axios.create(requestConfig);
+};
