486427512531b835e0b3ac6f3a1be22bb00a84eb
함초롬 <chorom.m.ham@gmail.com>
2021-11-21 00:11:52 +0900

    ✨ feat: Implement login

--- src/components/login.js
+++ src/components/login.js
@@ -10,10 +10,35 @@ import {
   FormLabel,
   Switch,
 } from "@chakra-ui/react";
+import logIn from "../services/login";
 
 export default function LogIn() {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [isTeacher, setIsTeacher] = useState(false);
+
+  const handleIdChange = (e) => {
+    setId(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const toggleIsTeacher = () => {
+    setIsTeacher(!isTeacher);
+  };
+
   const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
+
+  const handleLogInButtonClick = () => {
+    if (id === "" || password === "") {
+      alert("아이디와 비밀번호를 입력해주세요.");
+      return;
+    }
+    logIn(id, password, isTeacher);
+  };
 
   return (
     <Wrapper>
@@ -29,11 +54,22 @@ export default function LogIn() {
         <FormLabel htmlFor="is-teacher" mb="0">
           선생님이신가요?
         </FormLabel>
-        <Switch id="is-teacher" />
+        <Switch
+          id="is-teacher"
+          isChecked={isTeacher}
+          onChange={toggleIsTeacher}
+        />
       </FormControl>
-      <Input placeholder="아이디" mb="1rem" />
+      <Input
+        value={id}
+        onChange={handleIdChange}
+        placeholder="아이디"
+        mb="1rem"
+      />
       <InputGroup size="md">
         <Input
+          value={password}
+          onChange={handlePasswordChange}
           pr="4.5rem"
           type={show ? "text" : "password"}
           placeholder="비밀번호"
@@ -44,7 +80,12 @@ export default function LogIn() {
           </Button>
         </InputRightElement>
       </InputGroup>
-      <Button size="lg" colorScheme="blue" mt="2rem">
+      <Button
+        onClick={handleLogInButtonClick}
+        size="lg"
+        colorScheme="blue"
+        mt="2rem"
+      >
         로그인
       </Button>
     </Wrapper>
---
+++ src/services/login.js
@@ -0,0 +1,23 @@
+import base from "./airtable";
+
+export default async function logIn(id, password, isTeacher) {
+  const table = isTeacher ? "teacher" : "student";
+  await base(table)
+    .select({
+      filterByFormula: `{id} = '${id}'`,
+    })
+    .firstPage(function (err, records) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      if (records.length === 0) {
+        alert("아이디가 존재하지 않습니다.");
+      } else if (records[0].fields.password !== password) {
+        alert("비밀번호가 일치하지 않습니다.");
+      } else {
+        setCookie("id", records[0].fields.id);
+        location.replace = "/homeworks";
+      }
+    });
+}
