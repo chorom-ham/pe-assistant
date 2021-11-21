5056aa4851a57e7edf671c84cca4fa18d5c5087c
함초롬 <chorom.m.ham@gmail.com>
2021-11-21 14:39:17 +0900

    ✨ feat: Add react-query

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Head from "next/head";
 import styled from "styled-components";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 // eslint-disable-next-line no-unused-vars
 import * as tfjs from "@tensorflow/tfjs";
 import "@tensorflow/tfjs-backend-webgl";
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   const theme = extendTheme({
@@ -25,18 +28,20 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>체육 수업 도우미</title>
+        <title>체온: 체육 온라인</title>
         <link rel="shortcut icon" href="favicon.ico"></link>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, minimum-scale=1"
         ></meta>
       </Head>
-      <ChakraProvider theme={theme}>
-        <Main>
-          <Component {...pageProps} />
-        </Main>
-      </ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider theme={theme}>
+          <Main>
+            <Component {...pageProps} />
+          </Main>
+        </ChakraProvider>
+      </QueryClientProvider>
     </>
   );
 }
