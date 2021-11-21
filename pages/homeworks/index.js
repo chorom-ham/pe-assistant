a4b623f7c20f4050673b172535f9f15e1207358a
함초롬 <chorom.m.ham@gmail.com>
2021-11-21 15:41:31 +0900

    ✨ feat: Add homework list

---
+++ pages/homeworks/index.js
@@ -0,0 +1,10 @@
+import GlobalLayout from "src/layouts/global";
+import HomeworkList from "src/components/homework-list";
+
+export default function Home() {
+  return (
+    <GlobalLayout>
+      <HomeworkList />
+    </GlobalLayout>
+  );
+}
